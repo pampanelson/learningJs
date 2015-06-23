1d38ce1248794d059b218d13bb8258ac7f69fc17
pampanelson <pampa.nelson@gmail.com>
2015-06-23 15:45:20 +0800

    various ways of use js for 'class'

--- thisAndObj.js
+++ thisAndObj.js
@@ -1,1 +1,60 @@
-console.log('hello');
+console.log('hello,get wet with this and object of javascript');
+
+function MyObj (name) {
+    // body..
+    this.name = name;
+    // this.sayMyName = sayMyName;
+}
+
+MyObj.prototype.sayMyName = function() {
+    console.log(this);
+    console.log(this.name);
+};
+
+
+var myObj = new MyObj("Tom");
+// console.log(myObj.name);
+myObj.sayMyName();
+
+
+// function sayMyName () {
+//     console.log(this);
+//     console.log(this.name);
+// }
+// myObj.sayMyName();
+
+// // obj and function
+// function myFunction () {
+//     // body...
+//     //console.log('myFunction');
+// }
+
+// console.log(myFunction.constructor);
+
+// var myObj = {};
+
+// console.log(myObj.prototype);
+
+// properties and methods of object
+// var ParentObj = {
+//     str : 'parent Str',
+//     getMyStr : function getMyStr () {
+//         // console.log(this);
+//         return this.str;
+//     }
+// };
+
+// var ChildObj = Object.create(ParentObj);
+// ChildObj.getMyStr = function getChildStr () {
+//     return ParentObj.getMyStr() + " modified";
+// };
+// ChildObj.str = 'child Str';
+
+
+// var GrandChildObj = Object.create(ChildObj);
+// // GrandChildObj.str = 'grand child Str';
+
+// console.log(ParentObj.getMyStr());
+// console.log(ChildObj.getMyStr());
+// console.log(GrandChildObj.getMyStr());
+
